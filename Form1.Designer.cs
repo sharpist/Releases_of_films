72610c99fa9d38c103793ced7646a3461a4dd52e
Alexander Usov <alexusov@photographer.net>
2017-07-14 20:09:00 +0300

    Releases_of_films

---
+++ Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace Releases_of_films
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
+            this.buttonIn = new System.Windows.Forms.Button();
+            this.buttonOut = new System.Windows.Forms.Button();
+            this.textBoxOut = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxIn = new System.Windows.Forms.TextBox();
+            this.menu = new System.Windows.Forms.MenuStrip();
+            this.file = new System.Windows.Forms.ToolStripMenuItem();
+            this.exit = new System.Windows.Forms.ToolStripMenuItem();
+            this.help = new System.Windows.Forms.ToolStripMenuItem();
+            this.info = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonDel = new System.Windows.Forms.Button();
+            this.textBoxDel = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonIn
+            // 
+            this.buttonIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonIn.Location = new System.Drawing.Point(373, 258);
+            this.buttonIn.Name = "buttonIn";
+            this.buttonIn.Size = new System.Drawing.Size(100, 30);
+            this.buttonIn.TabIndex = 0;
+            this.buttonIn.Text = "Сохранить";
+            this.buttonIn.UseVisualStyleBackColor = true;
+            this.buttonIn.Click += new System.EventHandler(this.buttonIn_Click);
+            // 
+            // buttonOut
+            // 
+            this.buttonOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonOut.Location = new System.Drawing.Point(373, 333);
+            this.buttonOut.Name = "buttonOut";
+            this.buttonOut.Size = new System.Drawing.Size(100, 30);
+            this.buttonOut.TabIndex = 1;
+            this.buttonOut.Text = "Отобразить";
+            this.buttonOut.UseVisualStyleBackColor = true;
+            this.buttonOut.Click += new System.EventHandler(this.buttonOut_Click);
+            // 
+            // textBoxOut
+            // 
+            this.textBoxOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxOut.Location = new System.Drawing.Point(12, 149);
+            this.textBoxOut.MaximumSize = new System.Drawing.Size(350, 300);
+            this.textBoxOut.MinimumSize = new System.Drawing.Size(350, 300);
+            this.textBoxOut.Multiline = true;
+            this.textBoxOut.Name = "textBoxOut";
+            this.textBoxOut.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxOut.Size = new System.Drawing.Size(350, 300);
+            this.textBoxOut.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(82, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Релиз";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(238, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Фильм";
+            // 
+            // textBoxIn
+            // 
+            this.textBoxIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxIn.Location = new System.Drawing.Point(12, 77);
+            this.textBoxIn.MaximumSize = new System.Drawing.Size(350, 35);
+            this.textBoxIn.MinimumSize = new System.Drawing.Size(350, 35);
+            this.textBoxIn.Multiline = true;
+            this.textBoxIn.Name = "textBoxIn";
+            this.textBoxIn.Size = new System.Drawing.Size(350, 35);
+            this.textBoxIn.TabIndex = 5;
+            this.textBoxIn.Text = "\r\n";
+            this.textBoxIn.MouseEnter += new System.EventHandler(this.textBoxIn_MouseEnter);
+            // 
+            // menu
+            // 
+            this.menu.BackColor = System.Drawing.Color.Transparent;
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.file,
+            this.help});
+            this.menu.Location = new System.Drawing.Point(0, 0);
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(484, 24);
+            this.menu.TabIndex = 6;
+            this.menu.Text = "menuStrip1";
+            // 
+            // file
+            // 
+            this.file.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exit});
+            this.file.Name = "file";
+            this.file.Size = new System.Drawing.Size(48, 20);
+            this.file.Text = "Файл";
+            // 
+            // exit
+            // 
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(108, 22);
+            this.exit.Text = "Выход";
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // help
+            // 
+            this.help.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.info});
+            this.help.Name = "help";
+            this.help.Size = new System.Drawing.Size(68, 20);
+            this.help.Text = "Помощь";
+            // 
+            // info
+            // 
+            this.info.Name = "info";
+            this.info.Size = new System.Drawing.Size(148, 22);
+            this.info.Text = "Информация";
+            this.info.Click += new System.EventHandler(this.info_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(138, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Ввод данных";
+            // 
+            // buttonDel
+            // 
+            this.buttonDel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonDel.Location = new System.Drawing.Point(428, 425);
+            this.buttonDel.Name = "buttonDel";
+            this.buttonDel.Size = new System.Drawing.Size(45, 24);
+            this.buttonDel.TabIndex = 8;
+            this.buttonDel.Text = "Del";
+            this.buttonDel.UseVisualStyleBackColor = true;
+            this.buttonDel.Click += new System.EventHandler(this.buttonDel_Click);
+            // 
+            // textBoxDel
+            // 
+            this.textBoxDel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxDel.Location = new System.Drawing.Point(373, 425);
+            this.textBoxDel.MaximumSize = new System.Drawing.Size(45, 24);
+            this.textBoxDel.MinimumSize = new System.Drawing.Size(45, 24);
+            this.textBoxDel.Name = "textBoxDel";
+            this.textBoxDel.Size = new System.Drawing.Size(45, 24);
+            this.textBoxDel.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(368, 396);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Удалить фильм";
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxDel);
+            this.Controls.Add(this.buttonDel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxIn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxOut);
+            this.Controls.Add(this.buttonOut);
+            this.Controls.Add(this.buttonIn);
+            this.Controls.Add(this.menu);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menu;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(500, 500);
+            this.MinimumSize = new System.Drawing.Size(500, 500);
+            this.Name = "Form";
+            this.Text = "Релизы фильмов";
+            this.menu.ResumeLayout(false);
+            this.menu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonIn;
+        private System.Windows.Forms.Button buttonOut;
+        private System.Windows.Forms.TextBox textBoxOut;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxIn;
+        private System.Windows.Forms.MenuStrip menu;
+        private System.Windows.Forms.ToolStripMenuItem file;
+        private System.Windows.Forms.ToolStripMenuItem exit;
+        private System.Windows.Forms.ToolStripMenuItem help;
+        private System.Windows.Forms.ToolStripMenuItem info;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonDel;
+        private System.Windows.Forms.TextBox textBoxDel;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Releases_of_films
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+            // задать размеры для textBox
+            textBoxIn.ForeColor = Color.Gray;
+            textBoxDel.ForeColor = Color.Red;
+            textBoxIn.Text = "дд.мм.гггг - название";
+            textBoxIn.Size = new Size(this.Width - 350, 35);
+            textBoxOut.Size = new Size(this.Width - 350, 300);
+            textBoxDel.Size = new Size(this.Width - 45, 24);
+        }
+
+        private void exit_Click(object sender, EventArgs e) // выход из программы
+        { Application.Exit(); }
+
+        private void info_Click(object sender, EventArgs e) // инфо
+        {
+            this.TopMost = true;
+            MessageBox.Show("A simple program for reminding dates.\n© Alexander Usov");
+        }
+
+        private void textBoxIn_MouseEnter(object sender, EventArgs e) // событие 1 курсор внутри
+        {
+            textBoxIn.ForeColor = Color.Black;
+            textBoxIn.Text = "";
+            textBoxIn.MouseEnter -= textBoxIn_MouseEnter; // откл.
+        }
+        /*
+        private void textBoxIn_MouseLeave(object sender, EventArgs e) // событие 2 курсор снаружи
+        {
+            textBoxIn.ForeColor = Color.Gray;
+            textBoxIn.Text = "дд.мм.гггг - название";
+            //textBoxIn.MouseLeave -= textBoxIn_MouseLeave; // откл.
+        }
+        */
+        private void buttonIn_Click(object sender, EventArgs e) // сохранить
+        {
+            try
+            {
+                //if (!System.IO.File.Exists("Releases_of_films.txt")) { } // проверка на существование файла, не используется
+                string text = textBoxIn.Text; // записывает в переменную данные
+
+                FileStream file = new FileStream("Releases_of_films.txt", FileMode.Append, FileAccess.Write); // открывает поток
+                StreamWriter writer = new StreamWriter(file, Encoding.Unicode); // писатель
+
+                if (!String.IsNullOrWhiteSpace(text)) // проверка
+                {
+                    if (!(text == "дд.мм.гггг - название"))
+                    {
+                        if (text[10] == '-' || text[11] == '-' || text[12] == '-')
+                        {
+                            // форматирование перед записью
+                            string[] strValues = text.Split('\t', '\n', '\r', '.', '-'); // извлекает конкретную строку из массива
+                            string data = null;
+                            for (int t = 0; t < strValues.Length; t++)
+                            {
+                                data += $"{strValues[t].Trim(new Char[] { ' ', '*', '.' })}{(t < 2 ? '.' : ' ')}";
+                                if (t == 3) data = data.Insert(11, "- ");
+                                if (t == 3) data = data.TrimEnd(new Char[] { ' ' }) + "";
+                            }
+
+                            writer.WriteLine(data); // записывает в файл данные
+                            textBoxIn.Text = "";
+                        }
+                        else
+                        {
+                            textBoxIn.ForeColor = Color.Black;
+                            textBoxIn.Text = "Не соответствует формату!";
+                        }
+                    }
+                }
+                writer.Close(); // закрывает поток
+                buttonOut_Click(null, null);
+            }
+            catch (Exception ex)
+            {
+                textBoxOut.Text = ex.Message;
+            }
+        }
+
+        private void buttonOut_Click(object sender, EventArgs e) // отобразить
+        {
+            try
+            {
+                if (System.IO.File.Exists("Releases_of_films.txt")) // проверка на существование файла
+                {
+                    FileStream file = new FileStream("Releases_of_films.txt", FileMode.Open, FileAccess.Read); // открывает поток
+                    StreamReader reader = new StreamReader(file, Encoding.Unicode); // читатель
+
+                    string text = reader.ReadToEnd(); // в строку text записывает файл данных
+                    reader.Close(); // закрывает поток
+
+                    // создание массива union, согласование типов/формата данных
+                    string[] lines = text.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries); // заполнить массив разбитыми на табы строками
+                    //double[] realValues = new double[lines.Length]; // хранилище результатов, не используется
+                    int[][] union = new int [lines.Length][]; // хранилище результатов
+
+                    if (textBoxOut.Text != String.Empty) // обновить textBox перед выводом информации
+                    { textBoxOut.Text = ""; }
+
+                    for (int i = 0; i < lines.Length; i++)
+                    {
+                        string[] strValues = lines[i].Split('\t', '\n', '\r', '.', '-'); // извлекает конкретную строку из массива
+                        // форматирование строки
+                        string display = null;
+                        for (int t = 0; t < strValues.Length; t++)
+                        {
+                            display += $"{strValues[t].Trim(new Char[] { ' ', '*', '.' })}{(t < 2 ? '.' : ' ')}";
+                            if (t == 2) display += " «";
+                            else if (t == 3) display = display.TrimEnd(new Char[] { ' ' }) + "»";
+                        }
+                        // форматирование строки выполнено
+
+                        textBoxOut.Text += display; // вывод информации в textBox
+
+                        //realValues[i] = double.Parse(strValues[j]); // извлекает конкретный символ из строки
+                        union[i] = new int[strValues.Length - 1];
+                        for (int j = 0; j < strValues.Length; j++)
+                            if (!(j == 3))
+                                union[i][j] = Convert.ToInt32(strValues[j]);
+
+
+                        // вычислить даты
+                        DateTime dateNow = new DateTime();
+                        dateNow = DateTime.Now; // текущее время
+
+                        int y = 0, m = 0, d = 0;
+                        for (; i < union.Length;) // установка даты, используется массив union
+                        {
+                            for (int j = 0; j < union[i].Length; j++)
+                            {
+                                if (j == 0)
+                                    d = union[i][j];
+                                else if (j == 1)
+                                    m = union[i][j];
+                                else if (j == 2)
+                                    y = union[i][j];
+                            }
+                            break;
+                        }
+
+                        DateTime dateSet = new DateTime(y, m, d);  // установка год - месяц - день
+
+                        TimeSpan time = dateSet.Subtract(dateNow); // разница между датами
+
+                        int intact = time.Days + 1; // проверка
+                        string s = intact.ToString();
+                        char[] ar = s.ToCharArray();
+                        Array.Reverse(ar);
+                        s = new String(ar);
+                        int shift = Convert.ToInt32(s[0].ToString());
+                        if (shift == 1 && intact != 11) s = "день";
+                        else if (shift == 2 && intact != 12) s = "дня";
+                        else if (shift == 3 && intact != 13) s = "дня";
+                        else if (shift == 4 && intact != 14) s = "дня";
+                        else s = "дней";
+                        textBoxOut.Text += $"  через {time.Days + 1} {s}.{Environment.NewLine}"; // вывод информации в textBox
+                    }
+                }
+                else { textBoxOut.Text = "Информация не найдена!"; }
+            }
+            catch (Exception ex)
+            {
+                textBoxOut.Text = ex.Message;
+            }
+        }
+
+        private void buttonDel_Click(object sender, EventArgs e) // удалить
+        {
+            try
+            {
+                if (System.IO.File.Exists("Releases_of_films.txt")) // проверка на существование файла
+                {
+                    int del = Convert.ToInt32(textBoxDel.Text)-1; // номер индекса строки, которую надо удалить
+
+                    FileStream file = new FileStream("Releases_of_films.txt", FileMode.Open, FileAccess.Read); // открывает поток
+                    StreamReader reader = new StreamReader(file, Encoding.Unicode); // читатель
+
+                    string text = reader.ReadToEnd(); // в строку text записывает файл данных
+                    reader.Close(); // закрывает поток
+
+                    string[] lines = text.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries); // заполнить массив разбитыми на табы строками
+                    StreamWriter writer = new StreamWriter("Releases_of_films.txt", false, Encoding.Unicode); // писатель
+
+                    for (int i = 0; i < lines.Length; i++)
+                    {
+                        if (i == del)
+                            continue;
+                        writer.WriteLine(lines[i]);
+                    }
+                    writer.Close(); // закрывает поток
+                    buttonOut_Click(null, null);
+                }
+            }
+            catch (Exception ex)
+            {
+                textBoxOut.Text = ex.Message;
+            }
+        }
+    }
+}
